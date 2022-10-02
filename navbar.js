d2c46b679d5928545eda30b3d67322e17a8cbeac
tuomoAhonen <35593887+tuomoAhonen@users.noreply.github.com>
2022-10-02 12:36:07 +0300

    Add files via upload

--- navbar.js
+++ navbar.js
@@ -1,9 +1,18 @@
 function navbaropen() {
     var y = document.getElementById("navbuttons");
+    var x = document.getElementsByClassName("navigation-bar-button");
     if (y.style.display === "block") {
         y.style.display = "none";
+        x[0].style.display = "none";
+        x[1].style.display = "none";
+        x[2].style.display = "none";
+        x[3].style.display = "none";
     } else {
         y.style.display = "block";
+        x[0].style.display = "block";
+        x[1].style.display = "block";
+        x[2].style.display = "block";
+        x[3].style.display = "block";
     }
 }
 
